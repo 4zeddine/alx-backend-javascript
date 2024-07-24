a220d3e51d5abc2d979192af0d3450f97bbacda9
4zeddine <azeddine.nader@gmail.com>
2024-07-25 00:47:52 +0100

    task 6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,14 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((values) => {
+    values.map((v) => ({
+      status: v.status,
+      value: v.status === 'fulfilled' ? v.value : String(v.reason),
+    }));
+  });
+}
