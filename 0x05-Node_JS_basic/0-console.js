b27ba587875564a7a1f2f772411b5b6fce746bd9
4zeddine <azeddine.nader@gmail.com>
2024-09-17 14:56:08 +0100

    first commit

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,9 +1,8 @@
-/*
+/* *
 * Prints in STDOUT a string argument
-* @param:
-*   - str: the message to print
-* */
-const displayMessage = (str) =>{
+* @param:str the message to print
+*/
+const displayMessage = function(str){
     console.log(str);
 }
 module.exports = displayMessage;
